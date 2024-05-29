2cf2d44baaae40579e129d6ded7715842c682f69
joramgranger@outlook.com <joramgranger@outlook.com>
2024-05-29 11:13:25 -0700

    categories

--- src/data.js
+++ src/data.js
@@ -22,3 +22,21 @@ export const sliderItems = [
     },
 
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/14780455/pexels-photo-14780455.jpeg",
+        title: "BY CATEGORY"
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/14780455/pexels-photo-14780455.jpeg",
+        title: "BY SKIN CONCERN"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/14780455/pexels-photo-14780455.jpeg",
+        title: "BY SKIN TYPE"
+    },
+];

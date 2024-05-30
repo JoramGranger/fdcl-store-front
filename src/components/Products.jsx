09e005f4c4fc921bd1de219951c6244cc6c7f5d5
joramgranger@outlook.com <joramgranger@outlook.com>
2024-05-30 08:26:10 -0700

    products-c2

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import styled from 'styled-components';
-import ProductItem from './ProductItem';
+import Product from './Product';
 import { popularProducts } from '../data';
 
 const Container = styled.div`
     padding: 20px;
     display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
 `;
 
 const Products = () => {
   return (
     <Container>
         {popularProducts.map((item) =>(
-            <ProductItem item={item} key={item.id} />
+            <Product item={item} key={item.id} />
         ))}        
     </Container>
   )
